a270f9af35a39a0a11ab795348daf93105bcc8d4
mirandalily <mirandaraymond@me.com>
2016-03-16 18:46:31 -0700

    still tinkering with scraping html data, it only returns many instances of the same object at this point, when I would like different data

--- lib/gun_violence/cli.rb
+++ lib/gun_violence/cli.rb
@@ -11,11 +11,6 @@ class GunViolence::CLI
     @data.each.with_index(1) do |data, i|
       puts "#{i}. #{data.name}"
     end
-    @choice_data = GunViolence::Data.data_from_choice(@data)
-    @choice_data.each do |data|
-      puts "Date: #{data.date}, State: #{data.state}, City/County: #{data.city}, Deaths: #{data.deaths}, Injured: #{data.injured}, Source Link: #{data.source}"
-    end
-    binding.pry
   end
 
   def menu
@@ -26,9 +21,8 @@ class GunViolence::CLI
 
       if input.to_i > 0
         the_data = @data[input.to_i-1]
-        data_details = @choice_data
         puts "#{the_data.name}"
-        puts "#{data_details}"
+        @choice_data = GunViolence::Data.data_from_choice(the_data.url)
       elsif input == "list"
         list_violence_categories
       elsif input == "exit"
--- lib/gun_violence/data.rb
+++ lib/gun_violence/data.rb
@@ -4,21 +4,21 @@ class GunViolence::Data
 
   def self.scrape_data_choice
     doc = Nokogiri::HTML(open("http://www.gunviolencearchive.org/reports"))
-    choices = []
+    @choices = []
     doc.css(".reports-list .depth-1 a").map do |a|
       data = self.new
       data.name = a.text
-      data.url = "http://www.gunviolencearchive.com" << doc.search(".reports-list .depth-1 a").attr("href").value
-      choices << data
+      data.url = "http://www.gunviolencearchive.org" << doc.search(".reports-list .depth-1 a").attr("href").value
+      @choices << data
     end
-    choices.each.with_index(1) do |data, i|
+    @choices.each.with_index(1) do |data, i|
     end
   end
 
-  def self.data_from_choice(data)
-    doc = Nokogiri::HTML(open("http://www.gunviolencearchive.org/#{data.url}"))
-    occurances = []
-    doc.css("tbody").map do |a|
+  def self.data_from_choice(choice)
+    doc = Nokogiri::HTML(open(choice))
+    @occurances = []
+    doc.search("tbody tr").each do |x|
       data = self.new
       data.date = doc.css("td")[0].text
       data.state = doc.css("td")[1].text
@@ -26,9 +26,10 @@ class GunViolence::Data
       data.deaths = doc.css("td")[4].text
       data.injured = doc.css("td")[5].text
       data.source = doc.search(".links li.last a").attr("href").value
-      occurances << data
+      @occurances << data
     end
-    occurances.each do |data|
-    end
+    # @occurances.each.with_index(1) do |data, i|
+    #   puts " #{i} Date: #{data.date}, State: #{data.state}, City/County: #{data.city}, Deaths: #{data.deaths}, Injured: #{data.injured}, Source Link: #{data.source}"
+    # end
   end
 end
